89bb8c8bb098c184cfcad8d133fbe485f556153a
Pedro25 <1160779@isep.ipp.pt>
2020-02-29 21:46:56 +0000

    Progress on the spotlight instanciation function

---
+++ GameJam_2020/Assets/Scripts/ClickBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine("SpotlightCreation");
+        }
+    }
+
+    IEnumerator SpotlightCreation()
+    {
+        // Make a game object
+        GameObject lightGameObject = new GameObject("The Light");
+
+        // Add the light component
+        Light lightComp = lightGameObject.AddComponent<Light>();
+        lightComp.type = LightType.Spot;
+
+        // Set the position (or any transform property)
+        lightGameObject.transform.position = Input.mousePosition;
+
+        yield return new WaitForSeconds(2f);
+
+        //lightComp
+
+    }
+}
---
+++ GameJam_2020/Assets/Scripts/ProceduralNumberGenerator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralNumberGenerator {
+
+    static System.Random random = new System.Random();
+
+	public static int GetNextNumber() {
+        return random.Next(1,5);
+	}
+}
