8250bd3a6f9d41796d9db698e3f54fabbc9f5598
Pedro25 <1160779@isep.ipp.pt>
2020-02-29 20:22:15 +0000

    Fixed the spawn points; added end points; added new level funcionality

--- GameJam_2020/Assets/Scripts/PlayerSpawner.cs
+++ GameJam_2020/Assets/Scripts/PlayerSpawner.cs
@@ -7,14 +7,29 @@ public class PlayerSpawner: MonoBehaviour
     static System.Random random = new System.Random();
     public GameObject[] spawnPoints;
     public GameObject player;
+    public GameObject endPoint;
+    public int currentSpawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
         player.transform.position = chooseSpawnPoint().transform.position;
+        endPoint.transform.position = chooseEndPoint().transform.position;
     }
 
     private GameObject chooseSpawnPoint()
     {
-        return spawnPoints[random.Next(0, spawnPoints.Length)];
+        currentSpawnPoint = random.Next(0, spawnPoints.Length);
+        return spawnPoints[currentSpawnPoint];
+    }
+
+    private GameObject chooseEndPoint()
+    {
+        int currentEndPoint = currentSpawnPoint;
+        while (currentEndPoint == currentSpawnPoint)
+        {
+            currentEndPoint = random.Next(0, spawnPoints.Length);
+        }
+        return spawnPoints[currentEndPoint];
     }
 }
