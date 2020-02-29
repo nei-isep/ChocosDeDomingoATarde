3d56fe1648fc6fd4e9bb78de2a275efb71efd5cf
Francisco Leites <rhaelzo@outlook.pt>
2020-02-29 21:18:27 +0000

    Timer added, movement fixed and background changed

---
+++ GameJam_2020/Assets/Scripts/TextTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTime : MonoBehaviour
+{
+    float playTime = 120f;
+    public Text textBox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        playTime -= Time.deltaTime;
+        textBox.text = Mathf.RoundToInt(playTime).ToString();
+    }
+}
