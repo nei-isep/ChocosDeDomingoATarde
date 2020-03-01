e812c1d2c8a9454c2f5ae44e8371c7954a386934
Pedro25 <1160779@isep.ipp.pt>
2020-03-01 03:46:30 +0000

    Completed the spotlight implementation, all that is left it to connect it with the global timer

--- GameJam_2020/Assets/Scripts/ClickBehaviour.cs
+++ GameJam_2020/Assets/Scripts/ClickBehaviour.cs
@@ -1,39 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ClickBehaviour : MonoBehaviour
 {
+
+    public GameObject spotlight;
+    public static int noSpotlights = 0;
+    public static DateTime lastSpotlightTime = DateTime.MinValue;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //Instantiate(spotlight, new Vector3(71, 35, 28), spotlight.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetMouseButtonDown(0) && (lastSpotlightTime == DateTime.MinValue || (DateTime.Now - lastSpotlightTime).TotalSeconds >= 5))
         {
-            StartCoroutine("SpotlightCreation");
+            lastSpotlightTime = DateTime.Now;
+            noSpotlights++;
+            StartCoroutine(CreateSpotlight());
+
         }
     }
 
-    IEnumerator SpotlightCreation()
+    IEnumerator CreateSpotlight()
     {
         // Make a game object
-        GameObject lightGameObject = new GameObject("The Light");
+        GameObject lightGameObject = new GameObject("The Light" + noSpotlights.ToString());
 
         // Add the light component
         Light lightComp = lightGameObject.AddComponent<Light>();
         lightComp.type = LightType.Spot;
+        lightComp.intensity = 5;
+        lightComp.range = 50;
+        lightComp.spotAngle = 55;
+        lightComp.color = Color.white;
+        lightComp.shadows = LightShadows.Hard;
 
         // Set the position (or any transform property)
-        lightGameObject.transform.position = Input.mousePosition;
+        //https://answers.unity.com/questions/540888/converting-mouse-position-to-world-stationary-came.html
+        var v3 = Input.mousePosition;
+        v3.z = 60.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        lightGameObject.transform.position = new Vector3(v3.x, 35, v3.z);
+        lightGameObject.transform.eulerAngles = new Vector3(90, 0, 0);
 
         yield return new WaitForSeconds(2f);
 
-        //lightComp
-
+        if (lightGameObject != null)
+        {
+            GameObject.Destroy(lightGameObject);
+        }
     }
 }
